b69a938c6a1a46072a5453a0734ee83d777363cc
Mads Ohm Larsen <mads.ohm@gmail.com>
2013-06-27 11:23:56 +0200

    Reordered asset_sync

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,8 +1,9 @@
 AssetSync.configure do |config|
   config.fog_provider = 'Google'
+  config.fog_directory = ENV['FOG_DIRECTORY']
   config.google_storage_access_key_id = ENV['GOOGLE_STORAGE_ACCESS_KEY_ID']
   config.google_storage_secret_access_key = ENV['GOOGLE_STORAGE_SECRET_ACCESS_KEY']
-  config.fog_directory = ENV['FOG_DIRECTORY']
+  
   
   # Automatically replace files with their equivalent gzip compressed version
   config.gzip_compression = true
