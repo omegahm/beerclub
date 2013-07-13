04efd3dd9511a152ff7413f0db063903fe1bf9b9
Mads Ohm Larsen <mads.ohm@gmail.com>
2013-07-14 01:31:46 +0200

    Updated travis

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -11,5 +11,5 @@ AssetSync.configure do |config|
   # Fail silently.  Useful for environments such as Heroku
   config.fail_silently = true
 
-  config.custom_headers = { Vary: 'Accept-Encoding' }
+  config.custom_headers = "Vary: 'Accept-Encoding'"
 end
