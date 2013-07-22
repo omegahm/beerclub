e319acce0ae8dd81ce7c62530ea2f963e73be327
Mads Ohm Larsen <mads.ohm@gmail.com>
2013-07-22 18:52:42 +0200

    Disabled static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,12 +19,8 @@ Beerclub::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  # Needs to be false on Heroku
-  config.assets.initialize_on_precompile = false  
-
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
-  config.static_cache_control = "public, max-age=31536000"
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
