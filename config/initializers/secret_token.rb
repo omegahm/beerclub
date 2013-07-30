fce27a02e13fcbaef768a92a934495e540bc0366
Mads Ohm Larsen <mads.ohm@gmail.com>
2013-07-30 20:05:59 +0200

    Added secret token to ENV

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Beerclub::Application.config.secret_key_base = 'c4f981eb5eb1cbb9f142580a14f2b7cfc987be7f0e01df22c697485c58299ea457a6d2a82a0b6f8c8373deb2dee06b5217f76a462f308047230da29b236be4b8'
+Beerclub::Application.config.secret_key_base = ENV['SECRET_TOKEN'] || 'c4f981eb5eb1cbb9f142580a14f2b7cfc987be7f0e01df22c697485c58299ea457a6d2a82a0b6f8c8373deb2dee06b5217f76a462f308047230da29b236be4b8'
